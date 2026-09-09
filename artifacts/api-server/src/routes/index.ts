00b63a28a1268c6ea026371058c99bccfdc9be3f
SkinnytheHendrixx <theskinnyhendrixx@gmail.com>
2026-09-09 15:47:01 -0700

    Task #66 register execution recovery normalization

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -22,6 +22,7 @@ import humanActionsRouter from "./human-actions";
 import executionRouter from "./execution";
 import discoveryRouter from "./discovery";
 import { requireMoneyScoutAccess } from "../middlewares/authorizationMiddleware";
+import { normalizeExecutionRecoveryResolutionProblem } from "../middlewares/executionRecoveryResolutionMiddleware";
 import { requireSafePaidResearchRuntime } from "../middlewares/paidResearchSafetyMiddleware";
 import { registerApifyExperimentAdapters } from "../lib/apify-experiment-adapters";
 
@@ -33,6 +34,7 @@ router.use(healthRouter);
 router.use(authRouter);
 router.use(requireMoneyScoutAccess);
 router.use(requireSafePaidResearchRuntime);
+router.use(normalizeExecutionRecoveryResolutionProblem);
 router.use(opportunitiesRouter);
 router.use(evidenceRouter);
 router.use(evidenceWorkersRouter);
