df9c0481f53c75d673678206fed48af5e03979a5
SkinnytheHendrixx <theskinnyhendrixx@gmail.com>
2026-09-09 21:54:49 -0700

    Export asset operations schema

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -23,5 +23,6 @@ export * from "./human-actions";
 export * from "./execution";
 export * from "./build";
 export * from "./release";
+export * from "./asset";
 export * from "./runtime";
 export * from "./auth";
